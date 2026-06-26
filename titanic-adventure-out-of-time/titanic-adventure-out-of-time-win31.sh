f961df3ea81f5725bd25d553374bae15f687139b
appoloin <appoloin@gmail.com>
2026-06-26 21:16:00 +0100

    Competed Archive Game Files install

--- titanic-adventure-out-of-time/titanic-adventure-out-of-time-win31.sh
+++ titanic-adventure-out-of-time/titanic-adventure-out-of-time-win31.sh
@@ -1,112 +1,358 @@
 #!/bin/bash
 
-ROMs_FOLDER="$HOME/Games/ROMs/window3x"
+#Game        : Titantic Adventure Out of Time
+#
+#Platform    : Dosbox / Win3.11 
+#
+#Description : This script will create a Window 3.11 bottle and install the Game files.
+#              1: Get Game file(s) either an iso(s) or archive 
+#              2: Donwload a preinstalled Windows 3.11 bottle (url defined in WIN311_URL)
+#              3: Create a sub folder (name defined in GAME_NAME) in  ES-DE's ROMs folder (path defined in ROMs_FOLDER)
+#              4: Extreact Windows into a subfolder of ES-DE's ROMs folder (path defined in ROMs_FOLDER/GAME_NAME )
+#              5: Add folder named d to drives folder (ROMs_FOLDER/GAME_NAME/drives/d). Copy iso or extract archive 
+#                 into this new folder
+#              6: Downlaod Dosbox conf file from Github  (url defined in CONF_FILE)
+#              7: Run Dosbox using conf file, running the games install program
+
+
+#Constants
+ROMs_FOLDER="$HOME/Games/ROMs/windows3x"
 GAME_NAME="titianic_adventure_out_of_time.conf"
 WIN311_URL="https://www.dropbox.com/scl/fi/2b1x6cj30tdqq2me9kzv6/Win311.7z?rlkey=0xvdjybfq2242cry48huoyzsc&dl=0" 
+WIN311_NAME="Win311.7z"
+CONF_FILE_URL="https://raw.githubusercontent.com/appoloin/bash-scripts/refs/heads/main/titanic-adventure-out-of-time/titnanic-adventure-out-of-time.conf"
+CONF_FILE_NAME="titanic-adventure-out-of-time.conf"
+
+#Global
+FILES=""  #Game File Location
+RADIO_OPTION=0 #CD Image = 1;  Archive = 2
+
+
+#extract archive  
+#"$1" Archive Path
+#"S2" Output Path
+#"S3" Extract with (x) or Without Path (e) 
+#"S4" Password OPTIONAL
+extract_archive() {
+    local ARCHIVE_PATH="$1"
+    local OUTPUT="$2"
+    local EXTRACT_METHOD="$3"
+    local PASSWORD="$4"  # Optional: Password for encrypted archives
+
+    # Check if the archive file exists
+    if [ ! -f "$ARCHIVE_PATH" ]; then
+        zenity --error --text="Archive not found: $ARCHIVE_PATH"
+        exit 1
+    fi
+
+    # Extract the archive
+    if [[ "${EXTRACT_METHOD,,}" == "e" ]]; then 
+        #no paths
+        if [ -n "$PASSWORD" ]; then
+            # If a password is provided, use it for extraction
+            7z e -p"$PASSWORD" "$ARCHIVE_PATH" -o"$OUTPUT" -y
+        else
+            # Extract without a password
+            7z e "$ARCHIVE_PATH" -o"$OUTPUT" -y
+        fi
+    else       
+        #extract with paths                        
+        if [ -n "$PASSWORD" ]; then
+            # If a password is provided, use it for extraction
+            7z x -p"$PASSWORD" "$ARCHIVE_PATH" -o"$OUTPUT" -y
+        else
+            # Extract without a password
+            7z x "$ARCHIVE_PATH" -o"$OUTPUT" -y
+        fi
+    fi
+
+    # Check the exit status of 7z
+    case $? in
+        0)
+            if [[ "${EXTRACT_METHOD,,}" == "e" ]]; then 
+                #no paths
+                #Clean up after e extract (ie empty folders)
+                find "$OUTPUT" -type d -empty -delete
+            fi
+            ;;
+        1)
+            zenity --error --text="Warning: One or more files were not extracted \n$ARCHIVE_PATH"
+            exit 1
+            ;;
+        2)
+            zenity --error --text="Fatal error: Archive is corrupted or not a valid 7z file. \n$ARCHIVE_PATH"
+            exit 1
+            ;;
+        7)
+            zenity --error --text="Command-line error: Invalid arguments or syntax."
+            exit 1
+            ;;
+        8)
+            zenity --error --text="Not enough memory for the operation."
+            exit 1
+            ;;
+        9)
+            zenity --error --text="Archive is encrypted and no password was provided. \n$ARCHIVE_PATH"
+            exit 1
+            ;;
+        *)
+            zenity --error --text="Unknown error occurred while extracting the archive. \n$ARCHIVE_PATH"
+            exit 1
+            ;;
+    esac
+}
+
+
+#extract archive  
+#"$1" URL
+#"S2" Output Path
+#"S3" Output filename
+download_file() {
+    local URL="$1"
+    local OUTPUT_PATH="$2"
+    local OUTPUT_NAME="$3"  # Optional: Custom output filename
+
+    # Download the file
+    if [ -n "$OUTPUT_NAME" ]; then
+        # If a custom output filename is provided
+        wget -q -P "$OUTPUT_PATH" -O "$OUTPUT_PATH/$OUTPUT_NAME" "$URL"
+    else
+        # Default: Use the original filename
+        wget -q -P "$OUTPUT_PATH" "$URL"
+    fi
+
+    # Check if wget succeeded
+    if [ $? -ne 0 ]; then
+        zenity --error --text="Failed to download \nCheck the URL or your internet connection."
+        exit 1
+    fi
+}
 
 
 download_and_install_windows() {
-    #create game bottle
-    mkdir -p $ROMs_FOLDER/$GAME_NAME
 
     #get Win311 archive from dropbox
-    wget -P "$ROMs_FOLDER/$GAME_NAME"  -O "$ROMs_FOLDER/$GAME_NAME/Win311.7z" "$WIN311_URL" 
+    download_file "$WIN311_URL" "$ROMs_FOLDER/$GAME_NAME" "$WIN311_NAME" 
     # Check if wget succeeded
     if [ $? -ne 0 ]; then
-        echo "Failed to download: '$WIN311_URL'"
-        zenity --error --text="Failed to download: Win311.7z \nCheck the URL or your internet connection."
-        rm -f -r $ROMs_FOLDER/$GAME_NAME
         exit 1
     fi
 
     #extract to ROMs/Windows3x/Game folder  
-    7z x "$ROMs_FOLDER/$GAME_NAME/Win311.7z" -o"$ROMs_FOLDER/$GAME_NAME" -y
+    extract_archive "$ROMs_FOLDER/$GAME_NAME/$WIN311_NAME" "$ROMs_FOLDER/$GAME_NAME" "x"
     if [ $? -ne 0 ]; then
-        echo "Archive not found: $ROMs_FOLDER/$GAME_NAME/Win311.7z"
-        zenity --error --text="Archive not found: \n $ROMs_FOLDER/$GAME_NAME/Win311.7z"
-        rm -f -r $ROMs_FOLDER/$GAME_NAME
         exit 1
     fi
 
     #deleted downloaded zip
-    rm -f "$ROMs_FOLDER/$GAME_NAME/Win311.7z"
+    rm -f "$ROMs_FOLDER/$GAME_NAME/$WIN311_NAME"
+}
+
+
+#Get the loaction the archive files 
+select_archive() {
+    #Get ONLY ONE file
+    local FILE
+    local ARCHIVE_MIME='^application/(zip|x-tar|x-gzip|x-bzip2|x-7z-compressed|x-rar-compressed|x-xz)$'
+
+    FILE=$(zenity --file-selection \
+                  --multiple \
+                  --title="Select Game Archive" \
+                  --width=800 \
+                  --height=500 \
+                  --filename="$HOME/Downloads" \
+                  --file-filter="Archive | *.zip *.7z *.rar *.7z.001 *.001 *.zip.001 *.r00")
+
+    # Exit if user cancels
+    if [ $? -ne 0 ]; then 
+        exit 1 
+    fi
+    
+    local MIME_TYPE
+    MIME_TYPE=$(file --mime-type -b "$FILE")
+    echo "MIME_TYPE $MIME_TYPE"
+
+    # Check file's MIME type
+    if [[ ! $MIME_TYPE =~ $ARCHIVE_MIME ]]; then
+        echo "Wrong file type selected"
+        zenity --error --text="Error: '$FILE' \nis not a valid \n$ARCHIVE_MIME \nfile \n(MIME type: $MIME_TYPE)."
+        exit 1
+    fi
+
+    # Return the selected archive
+    FILES="$FILE"
+    return 0
+}
+
+
+#GET Location of CD images iso, cue/bin
+select_image_files() {
+    # Use zenity to select files
+    local files
+    files=$(zenity --file-selection \
+                   --multiple \
+                   --width=800 \
+                   --height=500 \
+                   --filename="$HOME/Downloads" \
+                   --title="Select CD image files" \
+                   --file-filter="CD IMAGE | *.iso *.cue *.bin *.mp3" )
+
+    # Exit if user cancels
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+
+    # Split the selected files into an array
+    local selected_files=($files)
+
+    # Check each file's extension
+    for file in "${selected_files[@]}"; do
+        if [[ ! "$file" == *."$file_type" ]]; then
+            zenity --error --text="Error: '$file' is not a valid $file_type file."
+            exit 1
+        fi
+    done
+
+    # Return the selected files
+    FILES="${selected_files[@]}"
 }
 
 
 
 
-#Get Game file
-FILE=$(zenity --file-selection \
-            --title="Select Game Archive" \
-            --width=800 \
-            --height=500 \
-            --filename="$HOME/Downloads" \
-            --file-filter="Archive | *.zip *.7z" \
-            --file-filter="Disc Image | *.iso" )
 
-if [ -z "$FILE" ]; then
-    echo "No file selected."
-    exit 1
-fi
+get_source_type () {
+    # Show the radio dialog
+    local SELECTED
+    SELECTED=$(zenity --list \
+                      --radiolist \
+                      --title="Select Source Type" \
+                      --text="Choose source of game files:" \
+                      --column="Select" \
+                      --column="Source" \
+                      FALSE "CD Image" \
+                      FALSE "Archive" )
+    
+
+    # Check if user canceled
+    if [ $? -ne 0 ]; then 
+        exit 1 
+    fi
+
+    case "$SELECTED" in
+        "CD Image")
+            RADIO_OPTION=1
+            ;;
+        "Archive")
+            RADIO_OPTION=2
+            ;;
+        *)
+            echo "Unknown selection: $SELECTED"
+            RADIO_OPTION=0
+            exit 1
+            ;;
+    esac
+
+}
 
 
-#Check for ISO or Zip
-if file --mime-type -b "$FILE" | grep -qE 'application/(zip|x-tar|x-gzip|x-bzip2|x-7z-compressed|x-rar-compressed|x-xz)'; then
-    #download win3 and install 
-    if $(download_and_install_windows) ; then
+main(){
+    get_source_type
+    if [ $? -ne 0 ]; then
+        echo "Error Selecting File"
+        exit 1
+    fi
+
+    if [[ $RADIO_OPTION -le 0 ]]; then
+        echo "Selected: $RADIO_OPTION"
+        zenity --error --text="Error: Selction Unknown : $RADIO_OPTION"
+        exit 1
+    elif  [[ $RADIO_OPTION -eq 1 ]]; then #CD IMAGE
+
+        select_image_files   #one or more files returned
+        if [ $? -ne 0 ]; then
+            echo "Error Selecting File"
+            exit 1
+        fi
+
+        #Create cdrom folder in Doxbox bottle
+        mkdir -p "$ROMs_FOLDER/$GAME_NAME/drives/d"
+
+        zenity --notification --text="Copying Files to ROMs folder" --title="Notification"
+
+        #copy iso to ROMs folder
+        cp "$FILE $ROMs_FOLDER/$GAME_NAME/drives/d"
+        if [$? -ne 0 ]; then
+            echo "COPY Failed. Exiting.\n  $FILE to  $ROMs_FOLDER/$GAME_NAME/drives/d"
+            zenity --error --text="COPY failed: \n   $FILE \n to  \n $ROMs_FOLDER/$GAME_NAME/drives/d"
+            #remove Game folder from ROMs/window3x directory
+            rm -f -r $ROMs_FOLDER/$GAME_NAME
+            exit 1
+        fi
+
+        zenity --notification --text="Copy Complete" --title="Notification"
+
+
+    elif  [[ $RADIO_OPTION -eq 2 ]]; then #Archive
+
+        select_archive  #Get Archive location
+        if [ $? -ne 0 ]; then
+            echo "Error Selecting File"
+            exit 1
+        fi
+
+        echo "Selected File $FILES"
+        if [ -z "$FILES" ]; then
+            echo "Selected File $FILES"
+        fi
+
+        #create game bottle
+        mkdir -p $ROMs_FOLDER/$GAME_NAME
+
+        echo "25"
+        
+        zenity --notification --text="Extracting Game Archive" --title="Notification"
+
+        extract_archive "$FILES" "$ROMs_FOLDER/$GAME_NAME/drives/d" "e"
+        if [ $? -ne 0 ]; then 
+            #remove Game folder from ROMs/window3x directory
+            rm -f -r $ROMs_FOLDER/$GAME_NAME
+            exit 1
+        fi
+        
+        zenity --notification --text="Extraction complete" --title="Notification"
+
+    fi
+
+    zenity --notification --text="Downloadind Windows 3.11 WFW" --title="Notification"
+
+    #download Windows 3 and install to ESDE ROMS Window3x folder 
+    download_and_install_windows
+    if [ $? -ne 0 ]; then
         echo "Download failed. Exiting."
-        exit 1
-    fi
-  
-    #extract archive 
-    7z e $FILE -o "$ROMs_FOLDER/$GAME_NAME/drives/d" -y
-    if [$? -ne 0 ]; then
-        echo "Extract failed. Exiting.\n  $FILE"
-        zenity --error --text="Archive not found: \n $FILE"
-        #remove Game folder from ROMs/window3x directory
-        rm $ROMs_FOLDER/$GAME_NAME
+        rm -f -r $ROMs_FOLDER/$GAME_NAME
         exit 1
     fi
 
-elif file --mime-type -b "$FILE" | grep -q "application/x-iso9660-image"; then
-    #download win3 and install
-    if $(download_and_install_windows) ; then
-        echo "Download failed. Exiting."
+    zenity --notification --text="Windows 3.11 WFW install complete" --title="Notification"
+
+    #Download conf file from github
+    download_file "$CONF_FILE_URL"  "$ROMs_FOLDER/$GAME_NAME" "$CONF_FILE_NAME"
+    # Check if wget succeeded
+    if [ $? -ne 0 ]; then
+        echo "Failed to download: '$CONF_FILE'"
+        rm -f -r $ROMs_FOLDER/$GAME_NAME
         exit 1
     fi
 
-    #copy iso to ROMs folder
-    mkdir -p $ROMs_FOLDER/$GAME_NAME/drives/d
-    if [$? -ne 0 ]; then
-        echo "MKDIR failed. Exiting.\n   $ROMs_FOLDER/$GAME_NAME/drives/d"
-        zenity --error --text="MKDIR failed: \n  $ROMs_FOLDER/$GAME_NAME/drives/d"
-        #remove Game folder from ROMs/window3x directory
-        rm $ROMs_FOLDER/$GAME_NAME
-        exit 1
-    fi
 
-    cp $FILE $ROMs_FOLDER/$GAME_NAME/drives/d
-    if [$? -ne 0 ]; then
-        echo "COPY Failed. Exiting.\n  $FILE to  $ROMs_FOLDER/$GAME_NAME/drives/d"
-        zenity --error --text="COPY failed: \n   $FILE \n to  \n $ROMs_FOLDER/$GAME_NAME/drives/d"
-        #remove Game folder from ROMs/window3x directory
-        rm $ROMs_FOLDER/$GAME_NAME
-        exit 1
-    fi
-
-else
-    zenity --info --text="The selected file is unknown."
-    exit 1
-fi
+    #run Game install
+    flatpak run io.github.dosbox-staging -conf "$ROMs_FOLDER/$GAME_NAME/$CONF_FILE_NAME"
+}
 
 
 
-#Download conf file from github
 
+main
 
-
-#run Game install
-flatpak run io.github.dosbox-staging -conf "$ROMs_FOLDER/$GAME_NAME/titanic-adventure-out-of-time.conf"
-
-
-
+exit
